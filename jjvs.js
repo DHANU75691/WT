97207b6c7ec7092dccf03c7307fef51404e361e0
DHANU75691 <s180541@rguktsklm.ac.in>
2022-08-03 11:07:21 +0100

    WT

---
+++ jjvs.js
@@ -0,0 +1,9 @@
+function sum(number1,number2)
+	{
+		return number1 + number2;
+	}
+	a=parseInt(prompt("enter a number"));
+	 b=parseInt(prompt("Enter another number"));
+	 document.write("the value is",sum(a,b));
+
+
---
+++ nz.js
@@ -0,0 +1,7 @@
+function myFunction(){
+var a = 5;
+var b = 6;
+var c = a + b;
+document.getElementById("demo").innerHTML =
+"The value of c is: " + c;
+}
